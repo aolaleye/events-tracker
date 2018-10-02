65f765b5dccb92ea973c21a4547400e80d3f2e01
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-10-02 15:40:27 -0400

    updated components

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -3,12 +3,17 @@ import { AuthService } from './auth.service';
 import { Router } from "@angular/router";
 
 @Component({
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    styles: [`
+        .error {border: 2px solid #e05c65;}
+        em { float: right; color: #e05c65; padding-left: 10px}
+    `]
 })
 
 export class LoginComponent {
     username
     password
+    mouseoverLogin
 
     constructor(private authService:AuthService, private router:Router) {}
 
